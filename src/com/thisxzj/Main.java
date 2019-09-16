bf43bbaa2390c7d5adffd8e675950abc4ec529f6
zjxu97 <zjxu97@126.com>
2019-09-16 21:37:12 +0800

    58

--- src/com/thisxzj/Main.java
+++ src/com/thisxzj/Main.java
@@ -1,39 +1,41 @@
 package com.thisxzj;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.HashMap;
+        import java.util.Map;
+        import java.util.Scanner;
+        import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int k = in.nextInt();
-        in.nextLine();
-        String line = in.nextLine();
-        String[] s = line.split(" ");
-        int length = s.length;
-        int[] nums = new int[length];
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        String[] numString = line.split(",");
+        int[] nums = new int[numString.length];
+        int length = nums.length;
         for (int i = 0; i < length; i++) {
-            nums[i] = Integer.parseInt(s[i]);
+            nums[i] = Integer.parseInt(numString[i]);
         }
-        System.out.println(func(nums, k));
-    }
-
-
-    public static int func(int[] A, int K) {
-        Arrays.sort(A);
-        int n = A.length;
-        int res = A[n - 1] - A[0];
-        for (int i = 1; i < n; i++) {
-            int i1 = A[0] + K;
-            int i2 = A[i] - K;
-            int i3 = A[n - 1] - K;
-            int i4 = A[i - 1] + K;
-
-            int min = i1 < i2 ? i1 : i2;
-            int max = i3 > i4 ? i3 : i4;
-            res = Math.min(max - min, res);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            Integer times = map.get(i);
+            map.put(i, times == null ? 1 : times + 1);
         }
-        return res;
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        int cnt = 0;
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            //System.out.println(key + " " + value);
+            if (value <= key + 1) {
+                cnt = cnt + key + 1;
+            } else {
+                while (value > 0) {
+                    cnt = cnt + key + 1;
+                    value = value - key - 1;
+                }
+            }
+        }
+        System.out.println(cnt);
     }
 }
