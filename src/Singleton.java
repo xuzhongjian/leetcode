79352c724f697cd4cb67ca2ee5f26b08b2ce5ffe
zjxu97 <zjxu97@126.com>
2019-09-20 09:19:31 +0800

    x

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,17 +1,20 @@
 /**
  * date    2019-09-20
  * time    08:48
- * 恶汉式 线程安全单例模式
+ * 静态内部类实现方式
  *
  * @author thisxzj - 中建
  */
 public class Singleton {
-    private static Singleton uniqueInstance = new Singleton();
 
-    public Singleton() {
+    private static class Inner {
+        private static final Singleton singleton = new Singleton();
     }
 
-    public Singleton getUniqueInstance() {
-        return uniqueInstance;
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        return Inner.singleton;
     }
 }
