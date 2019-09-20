5eff36adc7532841a022e53c0c361d00f7083a16
zjxu97 <zjxu97@126.com>
2019-09-20 08:53:33 +0800

    x

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,20 +1,17 @@
 /**
  * date    2019-09-20
  * time    08:48
- * 懒汉式，线程不安全单例模式
+ * 恶汉式 线程安全单例模式
  *
  * @author thisxzj - 中建
  */
 public class Singleton {
-    private static Singleton uniqueInstance;
+    private static Singleton uniqueInstance = new Singleton();
 
     public Singleton() {
     }
 
     public Singleton getUniqueInstance() {
-        if (uniqueInstance == null) {
-            uniqueInstance = new Singleton();
-        }
         return uniqueInstance;
     }
 }
