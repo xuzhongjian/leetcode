5b18804280cdceb2445490515edfa0d4b0f384b9
xuzhongjian <xuzhongjian@xiaomi.com>
2019-12-19 22:24:18 +0800

    init No.2

---
+++ src/com/xuzhongjian/leetcode/common/ListNode.java
@@ -0,0 +1,15 @@
+package com.xuzhongjian.leetcode.common;
+
+/**
+ * @author zjxu97
+ * @date 2019/12/19
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
