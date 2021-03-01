41df6b93b2492fcdd36bfbfa7066de6806e1cb05
xuzhongjian <xuzhongjian@xiaomi.com>
2021-03-02 01:10:33 +0800

    update
    update

--- editor/cn/[64]最小路径和.java
+++ editor/cn/[64]最小路径和.java
@@ -37,7 +37,24 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int[][] dp = new int[grid.length][grid[0].length];
-        
+
+        dp[0][0] = grid[0][0];
+        // 初始化左侧竖边
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        // 初始化上侧横边
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
