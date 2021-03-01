46deeaf514b69d31e332c68d0ed548a4733d13c6
xuzhongjian <xuzhongjian@xiaomi.com>
2021-03-02 01:25:36 +0800

    Update [46]全排列.java

--- editor/cn/[46]全排列.java
+++ editor/cn/[46]全排列.java
@@ -17,23 +17,32 @@
 
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    List<Integer> result = new ArrayList<>();
-
     public List<List<Integer>> permute(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num)
-        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backtrack(res, nums, new ArrayList<Integer>(), visited);
+        return res;
 
     }
 
-    public void subPermute(Set<Integer> set, List<Integer> pre) {
-
+    private void backtrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) continue;
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backtrack(res, nums, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
