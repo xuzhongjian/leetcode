f104a4c43bfaa99ff67aee5a69e799dca1b17fe6
xuzhongjian <xuzhongjian@xiaomi.com>
2021-03-02 15:48:41 +0800

    update
    update

--- editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
+++ editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
@@ -45,62 +45,46 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) {
+        int upper = upperBound(nums, target);
+        int low = lowerBound(nums, target);
+        //不存在情况
+        if (upper < low) {
             return new int[]{-1, -1};
         }
-        if (nums.length == 1 && nums[0] == target) {
-            return new int[]{0, 0};
-        }
-        int l = 0;
-        int r = nums.length - 1;
-        int mid = 0;
-        int frist = 0;
-        while (l < r) {
-            mid = l + (r - l) / 2;
-            if (mid == l || mid == r) {
-                break;
-            }
-            if (target > nums[mid]) {
-                l = mid;
-            } else if (target < nums[mid]) {
-                r = mid;
-            } else {
-                if (mid - 1 < 0 || nums[mid - 1] != target) {
-                    frist = mid;
-                    break;
-                } else {
-                    r = mid;
-                }
-            }
-        }
-        l = 0;
-        r = nums.length - 1;
-        mid = 0;
-        int last = -1;
-        while (l < r) {
-            mid = l + (r - l) / 2;
-            if (mid == l || mid == r) {
-                // add
-                break;
-            }
-            if (target > nums[mid]) {
-                l = mid;
-            } else if (target < nums[mid]) {
-                r = mid;
-            } else {
-                if (mid + 1 > nums.length - 1 || nums[mid + 1] != target) {
-                    last = mid;
-                    break;
-                } else {
-                    l = mid;
-                }
-            }
-        }
-        if (nums[frist] != target || nums[last] != target) {
-            return new int[]{-1, -1};
-        }
-        return new int[]{frist, last};
+        return new int[]{low, upper};
+    }
 
+    //计算下边界
+    int lowerBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            //这里需要注意，计算mid
+            int mid = left + ((right - left) >> 1);
+            if (target <= nums[mid]) {
+                //当目标值小于等于nums[mid]时，继续在左区间检索，找到第一个数
+                right = mid - 1;
+
+            } else if (target > nums[mid]) {
+                //目标值大于nums[mid]时，则在右区间继续检索，找到第一个等于目标值的数
+                left = mid + 1;
+
+            }
+        }
+        return left;
+    }
+
+    //计算上边界
+    int upperBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (target >= nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            }
+        }
+        return right;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
