5c3f8c7959176dc9d26ea62c4f644223bd531d79
xuzhongjian <xuzhongjian@xiaomi.com>
2021-03-05 12:29:50 +0800

    Update [221]最大正方形.java

--- editor/cn/[221]最大正方形.java
+++ editor/cn/[221]最大正方形.java
@@ -40,46 +40,41 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    private int[][] dp;
 
     public int maximalSquare(char[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        // 初始化最左上角
+        dp = new int[matrix.length][matrix[0].length];
         dp[0][0] = matrix[0][0] == '1' ? 1 : 0;
-        // 初始化左边
+        int max = dp[0][0];
         for (int i = 1; i < matrix.length; i++) {
-            dp[i][0] = matrix[i][0] == '1'
-                    ? dp[i - 1][0] + 1
-                    : 0;
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            max = Math.max(max, dp[i][0]);
         }
 
         // 初始化上边
         for (int i = 1; i < matrix[0].length; i++) {
-            dp[0][i] = matrix[0][i] == '1'
-                    ? dp[0][i - 1] + 1
-                    : 0;
+            dp[0][i] = matrix[0][i] == '1' ? 1 : 0;
+            max = Math.max(max, dp[0][i]);
         }
 
-        /*
-        for (int[] ints : dp) {
-            for (int i : ints) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
-        return 0;
-        */
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                dp[i][j] = dpFunc(matrix, i, j);
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                max = Math.max(max, dpFunc(matrix, i, j));
             }
         }
-        return dp[dp.length][dp[0].length];
+        return max * max;
     }
 
     public int dpFunc(char[][] matrix, int index1, int index2) {
-
-        return 0;
+        if (matrix[index1][index2] == '1') {
+            dp[index1][index2] = Math.min(dp[index1 - 1][index2 - 1], Math.min(dp[index1 - 1][index2], dp[index1][index2 - 1])) + 1;
+            if (index1 + 1 < dp[index1][index2] || index2 + 1 < dp[index1][index2]) {
+                dp[index1][index2] = Math.min(index1 + 1, index2 + 1);
+            }
+        } else {
+            dp[index1][index2] = 0;
+        }
+        return dp[index1][index2];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
